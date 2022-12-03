ef3a72721d992a14bb09a73ccd4bfd1bc67d944d
Haillander <haillanderviana@gmail.com>
2022-12-04 10:48:32 +1100

    Selectors added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-var = 
+
+// QuerySelector Representing elements from CSS //
+var containerE1= document.querySelector(".container");
+var viewScoresE1= document.querySelector(".view-scores");
+var timerE1= document.querySelector(".timer");
+var startQuizE1= document.querySelector(".start-quiz");
+var answersE1= document.querySelector(".answer");
+
+//Event Listeners
+startQuizE1.addEventListener('click', countdown);
+startQuizE1.addEventListener('click', questions);
+
+
+
+
+
+
+
+
+
+
+
+
+
