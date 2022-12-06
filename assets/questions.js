d7ad798cb7b3ac5e568d291c40fa3682aa7ca35c
Haillander <haillanderviana@gmail.com>
2022-12-06 16:55:38 +1100

    initial commit

---
+++ assets/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
+    },
+    {
+      title: "What are the two basic groups of data types in JavaScript?",
+      choices: [
+        "Primitive and attribute",
+        "Primitive and reference types",
+        "Reference types and attribute",
+        "None of the above"
+      ],
+      answer: "Primitive and reference types"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    }
+    ];
